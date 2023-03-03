e5538168e12b3aacdcbcf3e5e2fcc5657d00bd5a
クレイ <83293295+KureiLe@users.noreply.github.com>
2023-03-04 04:01:08 +0700

    idk why speechrecognition stuck on listening after like 4 time using...

--- main.py
+++ main.py
@@ -1,36 +1,60 @@
 import speech_recognition as sr
-import pyttsx3
 import keyboard
+import voicevox
+import googletrans
+import asyncio
+import playsound
+
 
 # Initialize the speech recognition and text-to-speech engines
 r = sr.Recognizer()
-engine = pyttsx3.init()
-
-# So that button click dont work when its in process
-inProcess = 0
+# Configure google translate real
+translator = googletrans.Translator()
 
 # Define a function to handle the button press event
-def button_press():
-    with sr.Microphone() as source:
-        inProcess = 1
-        print("Speak now...")
-        audio = r.listen(source)
-    try:
-        # Use the speech recognition engine to convert speech to text
-        voiceResult = r.recognize_google(audio)
-        print("You said: " + voiceResult)
-
-        engine.runAndWait()
-        inProcess = 0
-    except sr.UnknownValueError:
-        print("Speech recognition could not understand audio")
-        inProcess = 0
-    except sr.RequestError as e:
-        print("Error: " + str(e))
-        inProcess = 0
-
-if __name__ == "__main__":
+async def button_pressed():
+    # So that button click dont work when its in process
+    inProcess = 0
+    # File count
+    fileCount = 1
     while True:
         if keyboard.is_pressed("f") and inProcess == 0:
-            # Call the button_press() function to start listening for speech
-            button_press()
+            with sr.Microphone() as source:
+                inProcess = 1
+                print("Speak now...")
+                audio = r.listen(source, timeout=3, phrase_time_limit=20)
+            try:
+                # Use the speech recognition engine to convert speech to text
+                voiceResult = r.recognize_google(audio)
+                print("You said: " + voiceResult)
+
+                # Translate text
+                resultTranslated = translator.translate(voiceResult, dest="ja").text
+                print(f"Translated: {resultTranslated}")
+
+                # Put it to mp3 file
+                async with voicevox.Client() as client:
+                    audio_query = await client.create_audio_query(
+                        resultTranslated, speaker=20
+                    )
+                    with open(f"./results/result{fileCount}.wav", "wb") as f:
+                        f.write(await audio_query.synthesis())
+                        f.close()
+                
+                print(f"Done {fileCount}.wav")
+                # Play audio
+                playsound.playsound(f"./results/result{fileCount}.wav")
+                
+                fileCount += 1
+                inProcess = 0
+            except sr.UnknownValueError:
+                print("Speech recognition could not understand audio")
+                inProcess = 0
+            except sr.RequestError as e:
+                print("Error: " + str(e))
+                inProcess = 0
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(button_pressed())
+    loop.run_until_complete()
---
+++ readme.txt
@@ -0,0 +1,5 @@
+have to have the voicevox software running to make this work 
+
+sources:
+https://voicevox-client.tuna2134.jp/#requirements
+https://github.com/VOICEVOX/voicevox_engine/blob/master/README.md
